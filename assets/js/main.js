564ad4a68284dd1703d9a734a51b851c8d7ba4f0
snerva <104626636+snerva@users.noreply.github.com>
2022-10-20 13:45:43 +0200

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+/*creare un arraydi oggetti per rappresentare i membri del team. Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+MILESTONE 0: Creare l’array di oggetti con le informazioni fornite.
+MILESTONE 1: Stampare su console le informazioni di nome, ruolo e la stringa della foto
+MILESTONE 2: Stampare le stesse informazioni su DOM sottoforma di stringhe
+BONUS 1: Trasformare la stringa foto in una immagine effettiva
+BONUS 2: Organizzare i singoli membri in card/schede*/
+
+//creo una variabile const teamMembers con un array di oggetti = [{name:, role:, image: "img/..."}]
+
+//ciclo con forin in teamMembers per stampare le info richieste for (const key in teamMembers) { console.log(key, teamMembers[key]);}
+
+//creo un elemento in dom per stampare le informazioni richieste e le stampo come stringa inserendo spazi vuoti giá nelle stringhe Name: ' Wayne Barnett ', punteggiatura e a capo domEl.insertAdjacentHTML('beforeend', (key + ':' + value +'<br>')) }
+
+
