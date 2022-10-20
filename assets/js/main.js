8e4a2f79140775ee0dfafb60bda748a77f3b9ca7
snerva <selenenerva@live.it>
2022-10-20 15:36:10 +0200

    ciclo for per console log e forin per stampa video

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,17 +5,54 @@ MILESTONE 2: Stampare le stesse informazioni su DOM sottoforma di stringhe
 BONUS 1: Trasformare la stringa foto in una immagine effettiva
 BONUS 2: Organizzare i singoli membri in card/schede*/
 
+const domEl = document.querySelector('.info_strings');
 //creo una variabile const teamMembers con un array di oggetti = [{name:, role:, image: "img/..."}]
 const teamMembers = [
     {
         Name: ' Wayne Barnett ',
         Role: ' Founder & CEO ',
+        Image: ' wayne-barnett-founder-ceo.jpg '
+    },
+    {
+        Name: ' Angela Caroll ',
+        Role: ' Chief Editor ',
         Image: ' angela-caroll-chief-editor.jpg '
     },
+    {
+        Name: ' Walter Gordon ',
+        Role: ' Office Manager ',
+        Image: ' walter-gordon-office-manager.jpg '
+    },
+    {
+        Name: ' Angela Lopez ',
+        Role: ' Social Media Manager ',
+        Image: ' angela-lopez-social-media-manager.jpg '
+    },
+    {
+        Name: ' Scott Estrada ',
+        Role: ' Developer ',
+        Image: ' scott-estrada-developer.jpg '
+    },
+    {
+        Name: ' Barbara Ramos ',
+        Role: ' Graphic Designer ',
+        Image: ' barbara-ramos-graphic-designer.jpg '
+    }
 ]
 
-//ciclo con forin in teamMembers per stampare le info richieste for (const key in teamMembers) { console.log(key, teamMembers[key]);}
+//ciclo con for in teamMembers per stampare le info richieste for (let thisTeam per le teamMembers[i]) { console.log(thisTeam);}
 
-//creo un elemento in dom per stampare le informazioni richieste e le stampo come stringa inserendo spazi vuoti giá nelle stringhe Name: ' Wayne Barnett ', punteggiatura e a capo domEl.insertAdjacentHTML('beforeend', (key + ':' + value +'<br>')) }
+for(let i=0; i< teamMembers.length; i++){
+    let thisTeam = teamMembers[i];
+    console.log(thisTeam);
+    
+    //creo un elemento in dom per stampare le informazioni richieste e le stampo come stringa inserendo spazi vuoti giá nelle stringhe Name: ' Wayne Barnett ', punteggiatura e a capo domEl.insertAdjacentHTML('beforeend', (key + ':' + value +'<br>'))
+    for (const key in thisTeam) {
+        let value = thisTeam[key];
+        console.log(key + ':' + value);
+        domEl.insertAdjacentHTML('beforeend', (''+ key + ':' + value +'<br>')) 
+    }
+
+}
 
 
